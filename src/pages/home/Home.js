be3e14ddb62a457cbdbf3782faa1afc19fa1bf1d
LAPTOP-KE6E89HM\rinrada <nxmm19@gmail.com>
2024-10-20 23:30:27 +0700

    พิมไรก้อได้

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,17 +3,13 @@
 import React from "react";
 import "./HomeStyle.scss"; // นำเข้าไฟล์ SCSS
 
-
-
-
 export const Home = () => {
   return (
     <div className="home">
-      {" "}
-      {/* เพิ่ม className สำหรับการใช้สไตล์ */}
       <h1>Welcome to the Home Page!</h1>
       <p>This is a simple example of a home page using SCSS.</p>
       <h1>สวัสดี</h1>
+      <h1>น้ำหนึ่ง</h1>
     </div>
   );
 };
