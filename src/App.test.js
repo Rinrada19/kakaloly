adc2e224d10bdd80994b5cdd7dede9be416ffac3
namnueng32371 <namnueng.int@damrong.ac.th>
2025-01-07 15:30:35 +0700

    อะไรก็ได้

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import Summary_page from './pages/summary_page/Summary_page'
 
 test('renders learn react link', () => {
-  render(<Summary_page />);
+  render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
