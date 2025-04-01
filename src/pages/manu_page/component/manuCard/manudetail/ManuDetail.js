72cb9c776a7e13a47ac669ceeda2564ce79e070a
namnueng32371 <63924227+namnueng32371@users.noreply.github.com>
2025-04-01 22:47:18 +0700

    fix img

--- src/pages/manu_page/component/manuCard/manudetail/ManuDetail.js
+++ src/pages/manu_page/component/manuCard/manudetail/ManuDetail.js
@@ -9,8 +9,8 @@ import Freameicon from "../../../../../imgAll/element/Frameicon.png";
 import carbonicon from "../../../../../imgAll/element/carboicon.png";
 import oilicon from "../../../../../imgAll/element/oilicon.png";
 import potienicon from "../../../../../imgAll/element/potienicon.png";
-import salt from "../../../../../imgAll/element/salt.png";
-import sugar from "../../../../../imgAll/element/bagsugar.png";
+import salt from "../../../../../imgAll/element2/salt.png";
+import sugar from "../../../../../imgAll/element2/sugar.png";
 
 function MenuDetail() {
   const { id } = useParams(); // รับค่า id จาก URL
